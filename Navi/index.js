cbcb7dd04f43da87642143568943f7290f62cb10
Sydney Jeffries <43015804+SydneyJeffries@users.noreply.github.com>
2019-04-22 13:29:01 -0500

    Update index.js

--- Navi/index.js
+++ Navi/index.js
@@ -1,173 +1,67 @@
 
             
-// let varriables = {
-//  txt: "hi, i'm Sydney and i'm a web developer.",
-//  speed: 120,
-//  txt2: "I'm happy to meet you",
-//  contentBox: document.getElementById("contentBox"),
-//  blueButton: document.createElement('div'),
-//  buttonMake: function(){
-//     this.contentBox.appendChild(this.blueButton);
-//     this.blueButton.setAttribute('type', 'button');
-//     this.blueButton.setAttribute('class', 'button');
-//     this.blueButton.setAttribute('value', 'A');   
-//  }
-// }
-
-// // let i = 0 
-// // function writeLoop() {
-// //     if ( i < varriables.txt.length){ 
-// //         document.getElementById("contentBox").innerHTML += varriables.txt.charAt(i);
-// //             i++;
-// //             setTimeout(writeLoop, varriables.speed);
-// //             if (i == 34) {
-// //          setTimeout(varriables.buttonMake(), 5500);
-//  //          clearTimeout(); }
-// //           }
-// // }
-// // writeLoop();   
-    
-//  function typeGreetings() {
-//      varriables.blueButton.removeAttribute("class");
-//      //varriables.contentBox.removeChild(varriables.blueButton);
-//       varriables.contentBox.innerHTML= "";
-//       console.log("click worked")
-//         }
-
-// //    if (o < varriables.txt2.length){ 
-// //        contentBox.innerHTML += varriables.txt2.charAt(o);
-// //        o++;
-// //        setTimeout(typeGreetings, varriables.speed)
-// //       if (i == varriables.txt2.length) {
-// //         setTimeout(varriables.buttonMake(), 5500); }
-// //        };  
-// //     };
-  
- 
-// // varriables.blueButton.onclick =  typeGreetings;
-//   document.onclick = typeGreetings();
-
-
-// /////////////////////
-//   let  i = 0;
-//   var contentBox =  document.getElementById("contentBox");
-  
-  
-//   function buttonAppear() {
-//     let button = document.createElement('input');
-//      button.setAttribute('type', 'button');
-//      button.setAttribute('class', 'button');
-//      button.setAttribute('value', 'A');
-//     contentBox.appendChild(button) ;    
-//   };
-                
-   
-//   function typeHi() {
-//   var txt = "hi, i'm Sydney and i'm a web developer.";
-//   var speed = 120; 
-//   let txt2 = "I'm happy to meet you";
-         
-//    function LoopHi () {  
-//      if ( i < txt.length) { 
-//   document.getElementById("contentBox").innerHTML += txt.charAt(i);
-//       i++;
-//       setTimeout(typeHi, speed);
-//        if (i == 34) {
-//    setTimeout(buttonAppear(), 5500); }
-//        }
-//    }
-//    LoopHi();
-    
-//     var o = 0; 
-//    function typeGreetings () {
-//      let p = 0;
-//      if (p == 0) { 
-//        document.removeChild('button');
-//        contentBox.innerHTML= "";
-//          }
-  
-//      if (o < txt2.length){ 
-//         contentBox.innerHTML += txt2.charAt(o);
-//         o++;
-//         setTimeout(typeGreetings, speed)
-//        if (i == txt2.length) {
-//          setTimeout(buttonAppear(), 5500); }
-//         };  
-//    };
-    
-   
-//     button.onclick =  typeGreetings ();
-    
-//    button();
-//   };
-  
-//   typeHi();
-
-
-
-let  i = 0;
-let buttonID;   // = document.getElementById("theButton");
+let buttonID = document.getElementById("theButton");
+let buttonID2 = document.getElementById("theButton2");
 var contentBox =  document.getElementById("contentBox");
-let letThereBeButton;
-let txt2 = "I'm happy to meet you";
+let i = 0;
+let o = 0;
+let j = 0;
+
+LoopHi();
 
 function buttonAppear() {
-  let button = document.createElement('input');
-  buttonID = document.getElementById('button2');
-   button.setAttribute('type', 'button');
-   button.setAttribute('class', 'button');
-   button.setAttribute('id', 'button2')
-   button.setAttribute('value', 'A');
-   contentBox.appendChild(button);   
+  document.getElementById('theButton').style.visibility = "visible";
+}
 
-};
-
-// function buttonAppear(){
-// buttonID.style.opacity= "1" ;
-
-// }
-            
-
- 
- function LoopHi () {  
-  const txt = "hi, i'm Sydney and i'm a web developer.";
-   if ( i < txt.length) { 
+function LoopHi () {
+  const txt = "HEY! I'm Sydney and i'm a web developer.";
+  
+  if ( i < txt.length) { 
     document.getElementById("contentBox").innerHTML += txt.charAt(i);
     i++;
     setTimeout(LoopHi ,120);
-     if (i == 34) {
-    setTimeout(buttonAppear(), 5500);  
-        }
-     }
- } 
- LoopHi();
-   
-
-
-var o = 0; 
+  
+      if (i == txt.length) {
+        setTimeout(buttonAppear(), 5500);
+      }
+  }
+};
 
 function typeGreetings () {
-   contentBox.innerHTML= "";   
-  console.log('button clicked');
- if (o < txt2.length){ 
-    contentBox.innerHTML += txt2.charAt(o);
+  let txt = "LISTEN! I'm happy to meet you";
+
+  if ( o < txt.length ) {
+    contentBox.innerHTML += txt.charAt(o);
     o++;
     setTimeout(typeGreetings, 120)
-   if (o == txt2.length) {
-     setTimeout(buttonAppear(), 5500); }
-    };  
+    if (o == txt.length) {
+      buttonID2.style.visibility = "hidden";
+      // setTimeout(buttonAppear(), 5500); 
+    }
+  }
 };
 
 
 
 document.addEventListener('click', function  (event) {
   // If the clicked element doesn't have the right selector, bail
-  if (!event.target.matches('#button2')) return;
+  if (!event.target.matches('#theButton')) return;
   // Don't follow the link
   event.preventDefault();
+  contentBox.innerHTML= "";
+  buttonID.style.visibility = "hidden";
   typeGreetings();
   // Log the clicked element in the console
-  console.log(event.target)
+  console.log(event.target);
+}, false);
+
+document.addEventListener('click', function  (event) {
+  if (!event.target.matches('#theButton2')) return;
+  event.preventDefault();
+  contentBox.innerHTML= "";
+  buttonID2.style.visibility = "hidden";
+  
+  console.log(event.target);
 }, false);
 
 
